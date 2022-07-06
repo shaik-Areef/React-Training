65c035027e5e6739595cfaa246518dbdb1fb995b
areef <skareef403@gmail.com>
2022-07-06 13:35:18 +0530

    react-basic-project

---
+++ app1/app1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <OrderForm name='Ramesh'></OrderForm>
+      </header>
+    </div>
+  );
+}
+
+export default App;
