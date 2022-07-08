732c4fc452d2d6ce14f0d318080c3e928554c56e
areef <skareef403@gmail.com>
2022-07-08 18:10:43 +0530

    get data through API using given id

--- table/src/AlbumList.js
+++ table/src/AlbumList.js
@@ -4,22 +4,29 @@ import './AlbumList.css'
 function AlbumList() {
 
     let [photos, setPhtos] = useState([]);
+    let [albumid, setalbumid] = useState()
 
-    if (!photos.length) {
-        getphotos();
+
+    function onSearch() {
+        if (albumid > 0) {
+            getphotos();
+        }
     }
 
     async function getphotos() {
 
-        photos = await (await fetch("https://jsonplaceholder.typicode.com/photos?_page=1")).json();
+        photos = await (await fetch("https://jsonplaceholder.typicode.com/photos?_page=1&albumId=" + albumid)).json();
         console.log(photos);
         setPhtos(photos);
     }
     return (
         <div>
-            <h1> Album List</h1>
+            <h1> Showing Album List in Table Formate </h1>
+            <input type='number' placeholder="Enter Album ID" value={albumid} onChange={(e) => setalbumid(e.target.value)} ></input>
+            <button onClick={onSearch}>Search</button>
             <table>
                 <thead>
+                    <td>ID</td>
                     <td>Title</td>
                     <td>URL</td>
                 </thead>
@@ -27,8 +34,11 @@ function AlbumList() {
                     {
                         photos.map(function (photo) {
 
-                            return <tr><td>{photo.title}</td>
-                                <td>{photo.url}</td></tr>
+                            return <tr>
+                                <td>{photo.id}</td>
+                                <td>{photo.title}</td>
+                                <td><a href={photo.url}>Click</a></td>
+                            </tr>
                         })
                     }
                 </tbody>
