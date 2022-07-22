4e96d70a6cad7e49b0b8bae9b82474f82969463a
areef <skareef403@gmail.com>
2022-07-22 09:04:21 +0530

    SPINT update, adding Form Validations

--- Day-12 Final/keeptrack/src/Projects/Projects_page.tsx
+++ Day-12 Final/keeptrack/src/Projects/Projects_page.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React,{ useState, Fragment } from 'react';
 import { MOCK_PROJECTS } from './MockProject';
 import ProjectList from './ProjectList';
 import { Project } from './Project';
-function ProjectsPage() {
+
+const ProjectsPage = () => {
+    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
     const saveProject = (project: Project) => {
-            console.log('Saving project: ', project);
-          };
+        // console.log('Saving project: ', project);
+        let updatedProjects = projects.map((p: Project) => {
+            return p.id === project.id ? project : p;
+        });
+        setProjects(updatedProjects);
+    };
     return (
-        <div>
+        <Fragment>
             <h1>Projects</h1>
             {/* <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre> */}
-            <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} />
-        </div>
+            {/* <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} /> */}
+            <ProjectList onSave={saveProject} projects={projects} />
+        </Fragment>
 
     );
 
