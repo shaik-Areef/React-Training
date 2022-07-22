7e67b7c5e6719f8ca5aa44eca95f2f520e3ddc63
areef <skareef403@gmail.com>
2022-07-22 14:13:56 +0530

    Using Usecallback Demo

---
+++ Day-15/callback-demo/src/App.js
@@ -0,0 +1,30 @@
+import { useCallback, useState } from "react";
+import ReactDOM from "react-dom/client";
+import Todos from "./Todos";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  console.log('App call');
+  const increment = () => {
+
+    setCount((c) => c + 1);
+  };
+  const addTodo = useCallback(() => {    //apply UseCallback to aviod update to child component
+    setTodos((t) => [...t, "New Todo"]);
+  }, [todos]);
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
