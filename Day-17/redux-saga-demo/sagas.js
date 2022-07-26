c78245261ea4830d0d02804cbec92d4350a031bb
areef <skareef403@gmail.com>
2022-07-26 20:06:24 +0530

    SAGA Demo

---
+++ Day-17/redux-saga-demo/sagas.js
@@ -0,0 +1,27 @@
+import { put, takeEvery, all, call} from 'redux-saga/effects'
+
+export const delay = (ms) => new Promise(res => setTimeout(res, ms))
+
+
+
+// Our worker Saga: will perform the async increment task
+export function* incrementAsync() {
+    yield delay(1000)
+    yield put({ type: 'INCREMENT' })
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+export function* watchIncrementAsync() {
+    yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export function* helloSaga() {
+    console.log('Hello Sagas!')
+}
+
+export default function* rootSaga() {
+    yield all([
+        helloSaga(),
+        watchIncrementAsync()
+    ])
+}
